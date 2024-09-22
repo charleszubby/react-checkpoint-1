04198f693e27a312e8271c24301cbf97d6f44158
charles <ezecharles39@gmail.com>
2024-09-23 00:40:20 +0100

    react-checkoint

---
+++ src/product.js
@@ -0,0 +1,9 @@
+const product = {
+  name: "HP EliteBook 840",
+  price: 129.56,
+  description:
+    "HP EliteBook 840 is a laptop with a 14.00-inch display. It is powered by a Core i5 processor and it comes with 8GB of RAM. The HP EliteBook 840 packs 256GB of HDD storage. Connectivity options include Wi-Fi 802.11 a/b/g.",
+  image: "./laptop.jpeg",
+};
+
+export default product;
